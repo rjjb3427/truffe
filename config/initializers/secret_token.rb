309710a97379247266b410a7fdbf7b98eba6749a
natsu871 <rubymydear.n@gmail.com>
2012-03-19 15:25:28 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Truffe::Application.config.secret_token = '87a2c0296b59c0426cca7a5cf354529fd86369cb7a56c88dae78b182c5680a1a8fe30631ed0ed2f684fb62572fb333fd48dfd2653c7324b62ffdb67a5758cb96'
