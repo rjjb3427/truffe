52c06fdcf720af94df415ca4b2ab8de98fd6bfd6
Natsumi Hirano <natsumi@natsumi-iMac.local>
2012-07-25 01:51:12 +0900

    add basic_data model

---
+++ app/models/basic_data.rb
@@ -0,0 +1,2 @@
+class BasicData < ActiveRecord::Base
+end
---
+++ test/unit/basic_data_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class BasicDataTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
