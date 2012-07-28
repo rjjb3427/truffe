b53d805c443a8e723a0b5777c8052c16899fad4c
Natsumi Hirano <hirano@forif.co.jp>
2012-07-28 18:17:12 +0900

    add monthly_show to gigs

--- app/controllers/gigs_controller.rb
+++ app/controllers/gigs_controller.rb
@@ -1,2 +1,12 @@
 class GigsController < ApplicationController
+  def index
+    unless params[:month] || params[:year]
+      @gig_year  = Date.today.year
+      @gig_month = Date.today.month
+    else
+      @gig_year  = params[:year]
+      @gig_month = params[:month]
+    end
+    @gigs = Kaminari.paginate_array(Gigs.find(:all, :conditions => ['month(gig_date) = ? AND year(gig_date) = ?', @gig_month, @gig_year], :limit => 100, :order => 'gig_date asc')).page(params[:page])
+  end
 end
--- app/models/gigs.rb
+++ app/models/gigs.rb
@@ -1,2 +1,4 @@
 class Gigs < ActiveRecord::Base
+  default_scope :order => 'gig_date ASC'
+  paginates_per 30 
 end
