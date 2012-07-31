c0e91a2ddc742bead648ecc979cfc35f27c3be91
Natsumi Hirano <hirano@forif.co.jp>
2012-07-31 18:04:51 +0900

    change variables name of discographies

--- db/migrate/20120728104825_create_discographies.rb
+++ db/migrate/20120728104825_create_discographies.rb
@@ -2,12 +2,12 @@ class CreateDiscographies < ActiveRecord::Migration
   def change
     create_table :discographies do |t|
       t.date :release_date
-      t.string :album_name
+      t.string :title
       t.string :artist
       t.text :players
       t.string :record_co
-      t.string :shop_url
-      t.text :presentation
+      t.string :url
+      t.text :comment
       t.timestamps
     end
   end
