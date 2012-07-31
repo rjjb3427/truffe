b04393e7764603c61fda24e71be3cf9c868d840d
Natsumi Hirano <hirano@forif.co.jp>
2012-07-31 17:35:16 +0900

    update photos migration file

--- db/migrate/20120731053415_create_photos.rb
+++ db/migrate/20120731053415_create_photos.rb
@@ -1,7 +1,9 @@
 class CreatePhotos < ActiveRecord::Migration
   def change
     create_table :photos do |t|
-
+      t.date :photo_date
+      t.string :title
+      t.string :comment
       t.timestamps
     end
   end
