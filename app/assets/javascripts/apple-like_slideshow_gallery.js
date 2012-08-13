d18e2bd70d43c239bac2876c6a62fedc20c2006c
natsu871 <rubymydear.n@gmail.com>
2012-08-13 23:17:12 +0900

    update slideshow gallery js

--- app/assets/javascripts/apple-like_slideshow_gallery.js
+++ app/assets/javascripts/apple-like_slideshow_gallery.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
   
   $('#slides .slide').each(function(i){
     positions[i]= totWidth;
-    console.log('positions'+i+':'+positions[i]);
+    //console.log('positions'+i+':'+positions[i]);
     totWidth += $(this).width();
     if(!$(this).width()){
       alert("Please, fill in width & height for all your images!");
@@ -16,16 +16,15 @@ $(document).ready(function(){
   });
   
   $('#slides').width(totWidth);
-  console.log('totWidth:'+totWidth);
+  //console.log('totWidth:'+totWidth);
 
-
-  //$('#menu ul li').click(function(e,keepScroll){
+  //$('#menu li').click(function(e,keepScroll){
   $menuItem = $(".menuitem");
   $menuItem.on({
     "touchstart click" : function(e) {
     },
     "touchend click" : function(e,keepScroll) {
-      console.log('#menu ul li:clicked');
+      //console.log('#menu li:clicked');
       $menuItem.removeClass('act').addClass('inact');
       $(this).removeClass('inact').addClass('act');
       
@@ -37,12 +36,11 @@ $(document).ready(function(){
       
       // Stopping the auto-advance if an icon has been clicked:
       if(!keepScroll) clearInterval(itvl);
-                }
-        });
+    }
+  });
   
   /* On page load, mark the first thumbnail as active */
-  $('#menu ul li.menuitem:first').addClass('act').siblings().addClass('inact');
-  
+  $('#menu li.menuitem:first').addClass('act').siblings().addClass('inact');
   
   
   /*****
@@ -54,35 +52,27 @@ $(document).ready(function(){
   var current=1;
 
   function autoAdvance(){
-    console.log('called autoAdvance');
+    //console.log('called autoAdvance');
     if(current==-1) return false;
     
-    //slide_images(current);
-    $('#menu ul li').eq(current%$('#menu ul li').length).trigger('ontouchend',[true]);
+    slide_images(current);
+    $('#menu li').eq(current%$('#menu li').length).trigger('ontouchend',[true]);
     // [true] will be passed as the keepScroll parameter of the click function on line 28
     current++;
   }
 
-  var changeEvery = 3;
+  var changeEvery = 4;
   var itvl = setInterval(function(){autoAdvance()},changeEvery*1000);
 
-
-function slide_images(current) {
-  $obj = $('#menu ul li').eq(current%$('#menu ul li').length)
-  console.log('$obj:clicked');
-  $menuItem = $(".menuitem");
-  $menuItem.removeClass('act').addClass('inact');
-  $obj.addClass('act');
-  
-  var pos = $obj.prevAll('.menuitem').length;
-  
-  $('#slides').stop().animate({marginLeft:-positions[pos]+'px'},450);
-  
-  // Stopping the auto-advance if an icon has been clicked:
-//  if(!keepScroll) clearInterval(itvl);
-}
-
-
-
-
+  function slide_images(current) {
+    $obj = $('#menu li').eq(current%$('#menu li').length)
+    //console.log('$obj:clicked');
+    $menuItem = $('.menuitem');
+    $menuItem.removeClass('act').addClass('inact');
+    $obj.addClass('act');
+    
+    var pos = $obj.prevAll('.menuitem').length;
+    
+    $('#slides').stop().animate({marginLeft:-positions[pos]+'px'},550);
+  }
 });
