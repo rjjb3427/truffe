0a22e9a77cf07476282b4988b41f89eb1509648a
Natsumi Hirano <rubymydear.n@gmail.com>
2013-04-25 03:35:00 +0900

    add render successful test for gigs and discographies

--- spec/controllers/gigs_controller_spec.rb
+++ spec/controllers/gigs_controller_spec.rb
@@ -5,6 +5,11 @@ describe GigsController do
 
   describe "GET 'index'" do
 
+    it "should be successful" do
+      get :index
+      response.should be_success
+    end
+
     it "should have the right title" do
       get :index
       response.should have_selector("h1.page_title", :content => @page_title)
