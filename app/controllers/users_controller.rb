69d33c9450474769f9f26ba1d7498d47451977f0
Natsumi Hirano <rubymydear.n@gmail.com>
2013-05-08 03:09:46 +0900

    add gravatar image

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,11 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @page_title = @user.name
   end
 
   def new
+    @user = User.new
     @page_title = t('signup.title')
   end
 end
