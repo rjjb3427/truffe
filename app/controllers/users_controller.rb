f7c2cc8f39229e10c58aa7f74e78deaa1e713500
Natsumi Hirano <rubymydear.n@gmail.com>
2013-05-08 02:31:08 +0900

    add user validations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @page_title = t('signup.title')
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,26 +14,34 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :login_name, :email, :hashed_password
+  attr_accessor :password
+  attr_accessible :name, :email, :password, :password_confirmation
 
-  validates :login_name, :presence => true
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-  validates :hashed_password, :presence     => true,
+  validates :name,  :presence   => true,
+                    :length     => { :maximum => 50 }
+  validates :email, :presence   => true,
+                    :format     => { :with => email_regex },
+                    :uniqueness => { :case_sensitive => false }
+
+  validates :password, :presence  => true,
                               :confirmation => true,
                               :length       => { :within => 6..40 }
-  before_save :hashed_password
+
+  before_save :encrypt_password
 
   def has_password?(submitted_password)
-    hashed_password == encrypt(submitted_password)
+    encrypted_password == encrypt(submitted_password)
   end
 
+=begin
   def self.authenticate(email, submitted_password)
     user = find_by_email(email)
     return nil  if user.nil?
     return user if user.has_password?(submitted_password)
   end
 
-=begin
   def self.authenticate_with_salt(id, cookie_salt)
     user = find_by_id(id)
     (user && user.salt == cookie_salt) ? user : nil
@@ -42,12 +50,13 @@ class User < ActiveRecord::Base
 
   private
 
-  def hashed_password
-    self.hashed_password = encrypt(password)
+  def encrypt_password
+    self.salt = make_salt if new_record?
+    self.encrypted_password = encrypt(password)
   end
 
   def encrypt(string)
-    string
+    secure_hash("#{salt}--#{string}")
   end
 
   def make_salt
@@ -57,5 +66,4 @@ class User < ActiveRecord::Base
   def secure_hash(string)
     Digest::SHA2.hexdigest(string)
   end
-
 end
