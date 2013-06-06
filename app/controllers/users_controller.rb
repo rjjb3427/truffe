17a5bf628e898e61858779672fd7f1a5b3a4c073
Natsumi Hirano <rubymydear.n@gmail.com>
2013-06-07 06:55:24 +0900

    add authentication of edit/update pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_filter :authenticate, :only => [:edit, :update]
+  before_filter :correct_user, :only => [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -24,12 +26,10 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
     @page_title = t('settings.title')
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
       flash[:success] = t('settings.update')
       redirect_to @user
@@ -37,5 +37,16 @@ class UsersController < ApplicationController
       @page_title = t('settings.title')
       render 'edit'
     end
+  end
+
+  private
+
+  def authenticate
+    deny_access unless signed_in?
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -22,6 +22,14 @@ module SessionsHelper
     current_user = nil
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+
+  def deny_access
+    redirect_to signin_path, :notice => t('deny_access.messages.protected')
+  end
+
   private
 
   def user_from_remember_token
